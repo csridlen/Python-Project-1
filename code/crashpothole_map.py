d8ca394c3657c4bb2ff1ecc7e742041da1579e94
Steven Kim <94675002+su1214@users.noreply.github.com>
2022-03-19 15:12:03 -0500

    directory change, reformatted

--- code/crashpothole_map.py
+++ code/crashpothole_map.py
@@ -8,40 +8,44 @@ import shapefile
 import descartes
 
 if __name__ == "__main__":
-	os.makedirs('../artifacts', exist_ok=True)
+    os.makedirs("artifacts", exist_ok=True)
 
-	# load and plot chicago zipcodes 
-	zips = '../data/chicago_zipcodes.shp'
-	map_zips = gpd.read_file(zips)
-	fig,ax = plt.subplots(figsize = (15,15))
-	zip_plot = map_zips.plot(ax = ax, color='grey', alpha = 0.5)
+    # load and plot chicago zipcodes
+    zips = "data/chicago_zipcodes.shp"
+    map_zips = gpd.read_file(zips)
+    fig, ax = plt.subplots(figsize=(15, 15))
+    zip_plot = map_zips.plot(ax=ax, color="grey", alpha=0.5)
 
-	# load traffic crashes csv and filter for missing points
-	crash1 = pd.read_csv('../data/crashes_1.csv')
-	crash2 = pd.read_csv('../data/crashes_2.csv')
-	crashes = pd.concat([crash1, crash2])
-	crashes = crashes.loc[crashes['LATITUDE'] != 0]
-	crashes = crashes.loc[crashes['LONGITUDE'] != 0]
+    # load traffic crashes csv and filter for missing points
+    crash1 = pd.read_csv("data/crashes_1.csv")
+    crash2 = pd.read_csv("data/crashes_2.csv")
+    crashes = pd.concat([crash1, crash2])
+    crashes = crashes.loc[crashes["LATITUDE"] != 0]
+    crashes = crashes.loc[crashes["LONGITUDE"] != 0]
 
-	# load pothold data and filter for missing points
-	pot1 = pd.read_csv('../data/potholes_1.csv')
-	pot2 = pd.read_csv('../data/potholes_2.csv')
-	potholes = pd.concat([pot1, pot2])
-	potholes = potholes.loc[potholes['LONGITUDE'] != 0]
-	potholes = potholes.loc[potholes['LATITUDE'] != 0]
-	potholes = potholes.loc[potholes['STATUS'] == 'Open']
+    # load pothold data and filter for missing points
+    pot1 = pd.read_csv("data/potholes_1.csv")
+    pot2 = pd.read_csv("data/potholes_2.csv")
+    potholes = pd.concat([pot1, pot2])
+    potholes = potholes.loc[potholes["LONGITUDE"] != 0]
+    potholes = potholes.loc[potholes["LATITUDE"] != 0]
+    potholes = potholes.loc[potholes["STATUS"] == "Open"]
 
-	# create geodataframes 
-	crs = 'epsg:4326'
-	crash_geo = [Point(xy) for xy in zip(crashes['LONGITUDE'], crashes['LATITUDE'])]
-	crash_gdf = GeoDataFrame(crashes, crs = crs, geometry=crash_geo)
-	pothole_geo = [Point(xy) for xy in zip(potholes['LONGITUDE'], potholes['LATITUDE'])]
-	pothole_gdf = GeoDataFrame(potholes, crs = crs, geometry=pothole_geo)
+    # create geodataframes
+    crs = "epsg:4326"
+    crash_geo = [Point(xy) for xy in zip(crashes["LONGITUDE"], crashes["LATITUDE"])]
+    crash_gdf = GeoDataFrame(crashes, crs=crs, geometry=crash_geo)
+    pothole_geo = [Point(xy) for xy in zip(potholes["LONGITUDE"], potholes["LATITUDE"])]
+    pothole_gdf = GeoDataFrame(potholes, crs=crs, geometry=pothole_geo)
 
-	# plot crashes and potholes on zipcode map
-	map2 = crash_gdf.plot(ax = ax, markersize = 10, color = 'red', marker = 'o', label = 'crashes')
-	map22 = pothole_gdf.plot(ax = ax, markersize = 10, color = 'blue', marker = 'o', label = 'potholes')
+    # plot crashes and potholes on zipcode map
+    map2 = crash_gdf.plot(
+        ax=ax, markersize=10, color="red", marker="o", label="crashes"
+    )
+    map22 = pothole_gdf.plot(
+        ax=ax, markersize=10, color="blue", marker="o", label="potholes"
+    )
 
-	# save figure as png
-	mapf = map2.get_figure()
-	mapf.savefig("../artifacts/dualmap.png")
+    # save figure as png
+    mapf = map2.get_figure()
+    mapf.savefig("artifacts/dualmap.png")
