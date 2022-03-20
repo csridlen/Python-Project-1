de4a7a1a583ce257b37ac26ae4e93355a6b7f29b
brendanok <brendanhok@gmail.com>
2022-03-20 17:52:15 -0500

    made a map that shows the open potholes color coded by days elapsed since service request was created

--- code/open_potholes_map.py
+++ code/open_potholes_map.py
@@ -23,3 +23,12 @@ open_potholes = potholes.loc[potholes['STATUS'] == 'Open']
 open_potholes['DAYS_OPEN'] = (pd.to_datetime('today').normalize() - potholes['CREATION DATE']).dt.days
 
 # Interestingly, the average time that these service requests have been open are much longer than the average time it takes to complete a pothole.
+
+
+# This map shows a scatter of every open pothole, but you can make another one grouped it by zipcode
+fig = px.scatter_mapbox(open_potholes, lat="LATITUDE", lon="LONGITUDE", mapbox_style="carto-positron",
+                        color="DAYS_OPEN",
+                        color_continuous_scale=["green", 'blue', 'red', 'gold'],
+                        range_color=[0, open_potholes['TIME_TO_COMPLETE'].quantile(0.95)],
+                        height=700, zoom=9)
+fig.show()
